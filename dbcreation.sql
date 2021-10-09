527ad75ce94870d44096afae9a3129d3a19a7290
Joao Ramalho <joaodramalho@gmail.com>
2021-10-09 19:04:10 +0100

    bug fixes

--- dbcreation.sql
+++ dbcreation.sql
@@ -2,6 +2,8 @@ USE master
 GO
 CREATE DATABASE Blog
 GO
+USE Blog
+GO
 CREATE TABLE [User]
 (
     user_id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
@@ -33,11 +35,12 @@ CREATE TABLE Posts
 GO
 ALTER TABLE Posts
 ADD CONSTRAINT fk_user_id
-REFERENCES [User].user_id
+FOREIGN KEY (user_id)
+REFERENCES [User](user_id)
 GO
 CREATE TABLE Comments
 (
-    comment_id INT NOT NULL PRIMARY KEY,
+    [comment_id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     post_id INT NOT NULL,
     user_id INT NOT NULL,
     content NVARCHAR(max) NOT NULL,
@@ -47,11 +50,14 @@ CREATE TABLE Comments
     user_change NVARCHAR(30) NOT NULL,
     date_change DATETIME NOT NULL
 )
+GO
 ALTER TABLE Comments
 ADD CONSTRAINT fk_post_id
-REFERENCES Posts.post_id
+FOREIGN KEY (post_id)
+REFERENCES Posts(post_id)
 GO
-ALTER TABLE Comment
+ALTER TABLE Comments
 ADD CONSTRAINT fk_posts_user_id
-REFERENCES [User].user_id
+FOREIGN KEY (user_id)
+REFERENCES [User](user_id)
 GO
